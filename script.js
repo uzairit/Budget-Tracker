b2a9548fd50aaf56ff11ff5689d6566721f27072
uzairit <uzair18it@gmail.com>
2025-01-27 07:37:23 +0500

    update

--- script.js
+++ script.js
@@ -19,24 +19,29 @@ let cAmount = document.getElementById('cAmount').addEventListener('click', funct
 
   let pCost = parseFloat(productCost.value);
   let budgetValue = parseFloat(budget.value);
-  if (pCost > budgetValue) {
+
+  let newExpences = parseFloat(expences.innerHTML) + pCost;
+
+  if (newExpences > budgetValue) {
     alert('Product cost exceeds available budget!');
     return;
   }
 
+  
   let tr = document.createElement('tr');
   tr.innerHTML = `
     <td class="pName">${pName.value}</td>
     <td class="Ammount">${productCost.value}</td>
     <td><button class="del">Delete</button></td>`;
   pTable.appendChild(tr);
-
-  let newExpences = parseFloat(expences.innerHTML) + pCost;
+  
   expences.innerHTML = newExpences;
   balance.innerHTML = budgetValue - newExpences;
 
   pName.value = '';
   productCost.value = '';
+
+  
 
   let delBtn = tr.querySelector('.del');
   delBtn.addEventListener('click', function () {
